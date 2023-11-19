0311f5e7380f8af659ceee6b9c402f104e2c92cb
Anastasia Byalaya <barybka_jack_jack_kusto@vk.com>
2023-11-20 01:10:28 +0700

    Homework yt_an_pr done

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,25 @@
+import json
+import os
+
+from googleapiclient.discovery import build
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    API_KEY: str = os.getenv("API_KEY")
+    youtube = build("youtube", "v3", developerKey=API_KEY)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.channel = self.youtube.channels().list(id=channel_id, part="snippet,statistics").execute()
 
-    def print_info(self) -> None:
+    def print_info(self) -> str:
         """Выводит в консоль информацию о канале."""
-        pass
+        # return json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute())
+        return json.dumps(
+            self.youtube.channels().list(id=self.channel_id, part="snippet,statistics").execute(),
+            indent=2,
+            ensure_ascii=False,
+        )
